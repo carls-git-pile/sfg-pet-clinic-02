03a2e46837445015c7c0e3b738555c0fb24a1640
carl <carl@localhost.localdomain>
2020-11-02 20:01:27 -0500

    project init w/ initializer

---
+++ src/main/java/com/example/sfgpetclinic02/SfgPetClinic02Application.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinic02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinic02Application.class, args);
+	}
+
+}
