c655964e802c3acd86ad9c86518c9548200d6fb0
carl <carl@localhost.localdomain>
2020-11-03 14:45:31 -0500

    refactor to multi-module project.  Closes #1.

---
+++ pet-clinic-web/src/test/java/sfgpetclinic02/SfgPetClinic02ApplicationTests.java
@@ -0,0 +1,13 @@
+package sfgpetclinic02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class SfgPetClinic02ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
