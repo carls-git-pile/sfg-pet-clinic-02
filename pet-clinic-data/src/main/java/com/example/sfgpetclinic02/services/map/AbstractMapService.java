a1cef45846b8ab307001a954e3607e128959a375
carl <carl@localhost.localdomain>
2020-12-12 14:37:38 -0500

    add tests for OwnerMapSvc. Closes #49

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic02/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic02/services/map/AbstractMapService.java
@@ -17,8 +17,10 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
     T save(T object) {
 
-        if (object != null && object.getId() == null) {
-            object.setId(getNextId());
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
             map.put(object.getId(), object);
         } else {
             throw new RuntimeException("Object cannot be null");
